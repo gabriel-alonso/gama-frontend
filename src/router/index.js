1755bfa44a97c7a5d0a655c1d01896957316afc9
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2022-10-24 20:02:53 -0300

    fix: update router index

--- src/router/index.js
+++ src/router/index.js
@@ -21,65 +21,16 @@ const routes = [
     meta: { breadCrumb: 'Requests' }
   },
   {
-    path: '/typography',
-    name: 'typography',
-    component: () => import('@/views/typography/Typography.vue'),
-    meta: { breadCrumb: 'Typography' }
+    path: '/scan',
+    name: 'scan',
+    component: () => import('@/views/scan/Scan.vue'),
+    meta: { breadCrumb: 'Scan' }
   },
   {
-    path: '/icons',
-    name: 'icons',
-    component: () => import('@/views/icons/Icons.vue'),
-    meta: { breadCrumb: 'Icons' }
-  },
-  {
-    path: '/cards',
-    name: 'cards',
-    component: () => import('@/views/cards/Card.vue'),
-    meta: { breadCrumb: 'Cards' }
-  },
-  {
-    path: '/simple-table',
-    name: 'simple-table',
-    component: () => import('@/views/simple-table/SimpleTable.vue'),
-    meta: { breadCrumb: 'Simple Table' }
-  },
-  {
-    path: '/form-layouts',
-    name: 'form-layouts',
-    component: () => import('@/views/form-layouts/FormLayouts.vue'),
-    meta: { breadCrumb: 'Form Layouts' }
-  },
-  {
-    path: '/pages/account-settings',
-    name: 'pages-account-settings',
-    component: () =>
-      import('@/views/pages/account-settings/AccountSettings.vue'),
-    meta: { breadCrumb: 'Configurações de conta' }
-  },
-  {
-    path: '/pages/login',
-    name: 'pages-login',
-    component: () => import('@/views/pages/Login.vue'),
-    meta: {
-      layout: 'blank'
-    }
-  },
-  {
-    path: '/pages/register',
-    name: 'pages-register',
-    component: () => import('@/views/pages/Register.vue'),
-    meta: {
-      layout: 'blank'
-    }
-  },
-  {
-    path: '/error-404',
-    name: 'error-404',
-    component: () => import('@/views/Error.vue'),
-    meta: {
-      layout: 'blank'
-    }
+    path: '/scan-data',
+    name: 'scan-data',
+    component: () => import('@/views/scanned-data/ScanData.vue'),
+    meta: { breadCrumb: 'Scan' }
   },
   {
     path: '*',
