24bf71194cd1f87a08c8f9e6974a019df4459ae4
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2022-10-24 23:25:45 -0300

    fix: update scandata

---
+++ src/views/dashboard/ListScanData.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-card>
+    <v-card-title class="align-start">
+      <span>Scan Data</span>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="usreList"
+      item-key="full_name"
+      class="table-rounded"
+      hide-default-footer
+      disable-sort
+    >
+      <!-- name -->
+      <template #[`item.full_name`]="{item}">
+        <div class="d-flex flex-column">
+          <span
+            class="d-block font-weight-semibold text--primary text-truncate"
+            >{{ item.full_name }}</span
+          >
+          <small>{{ item.post }}</small>
+        </div>
+      </template>
+      <template #[`item.salary`]="{item}">
+        {{ `$${item.salary}` }}
+      </template>
+      <!-- status -->
+      <template #[`item.status`]="{item}">
+        <v-chip
+          small
+          :color="statusColor[status[item.status]]"
+          class="font-weight-medium"
+        >
+          {{ status[item.status] }}
+        </v-chip>
+      </template>
+    </v-data-table>
+  </v-card>
+</template>
+
+<script>
+import { mdiSquareEditOutline, mdiDotsVertical } from '@mdi/js'
+import data from './datatable-data'
+
+export default {
+  setup() {
+    const statusColor = {
+      /* eslint-disable key-spacing */
+      Current: 'primary',
+      Professional: 'success',
+      Rejected: 'error',
+      Resigned: 'warning',
+      Applied: 'info'
+      /* eslint-enable key-spacing */
+    }
+
+    return {
+      headers: [
+        { text: 'NOME', value: 'full_name' },
+        { text: 'EMAIL', value: 'email' },
+        { text: 'CPF', value: 'cpf' },
+        { text: 'TIPO', value: 'type' },
+        { text: 'STATUS', value: 'status' }
+      ],
+      usreList: data,
+      status: {
+        1: 'Current',
+        2: 'Professional',
+        3: 'Rejected',
+        4: 'Resigned',
+        5: 'Applied'
+      },
+      statusColor,
+
+      // icons
+      icons: {
+        mdiSquareEditOutline,
+        mdiDotsVertical
+      }
+    }
+  }
+}
+</script>
---
+++ src/views/scanned-data/ScanData.vue
@@ -0,0 +1,98 @@
+<template>
+  <v-col>
+    <v-row>
+      <v-col>
+        <H1>Lista de ScansData</H1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-expansion-panels>
+          <v-expansion-panel>
+            <v-expansion-panel-header disable-icon-rotate color="primary">
+              <template v-slot:actions>
+                <v-icon color="white">
+                  mdi-magnify
+                </v-icon>
+              </template>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <v-row class="mt-1">
+                <v-col cols="12" lg="4" md="4" sm="4">
+                  <v-text-field
+                    label="ID"
+                    outlined
+                    dense
+                    placeholder="ID"
+                    hide-details="auto"
+                  />
+                </v-col>
+                <v-col cols="12" lg="4" md="4" sm="4">
+                  <v-text-field
+                    label="Valor"
+                    outlined
+                    dense
+                    placeholder="Valor"
+                    hide-details="auto"
+                  />
+                </v-col>
+                <v-col cols="12" lg="4" md="4" sm="4">
+                  <v-text-field
+                    label="Valor de Referência"
+                    outlined
+                    dense
+                    placeholder="Valor de Referência"
+                    hide-details="auto"
+                  />
+                </v-col>
+                <v-col cols="12" lg="4" md="4" sm="4">
+                  <v-text-field
+                    label="Tipo"
+                    outlined
+                    dense
+                    placeholder="Tipo"
+                    hide-details="auto"
+                  />
+                </v-col>
+              </v-row>
+
+              <v-row>
+                <v-col class="d-flex justify-end">
+                  <v-btn class="mr-2" color="secondary">
+                    Limpar
+                  </v-btn>
+
+                  <v-btn color="primary">
+                    Pesquisar
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <dashboard-datatable />
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <v-pagination :length="6" :value="1" />
+      </v-col>
+    </v-row>
+  </v-col>
+</template>
+
+<script>
+import DashboardDatatable from './DashboardDatatable.vue'
+
+export default {
+  components: {
+    DashboardDatatable
+  }
+}
+</script>
